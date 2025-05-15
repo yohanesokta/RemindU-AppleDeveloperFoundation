7a95796c24693eaf1c1be1c9b50050ff19e84598
gio <giovanniadis@icloud.com>
2025-05-15 10:54:00 +0700

    BUAT SPLASH SCREEN

---
+++ SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  remindme
+//
+//  Created by MacBook on 14/05/25.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
