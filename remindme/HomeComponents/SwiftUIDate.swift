d658b7b1ca0629efeebd982dabc291894c19d872
Yohanes Oktanio <yohanesoktayt72@gmail.com>
2025-05-15 11:08:17 +0700

    feat : Selected Children And Adult

---
+++ remindme/HomeComponents/SwiftUIDate.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIDate.swift
+//  remindme
+//
+//  Created by MacBook on 15/05/25.
+//
+
+import SwiftUI
+
+struct SwiftUIDate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIDate()
+}
